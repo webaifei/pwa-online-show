af93b1c66ea497631d8b9d785147193432c7f721
zhangaifei <zhangaifei@51.com>
2018-09-19 16:49:38 +0800

    init project

---
+++ use-pwa/precache-manifest.4d1eb0016e72f084f206887a5c7bc8dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ccb1d7f108416bfd8a4e",
+    "url": "/js/chunk-vendors.343b7725.js"
+  },
+  {
+    "revision": "445e7f53c89b3dc22884",
+    "url": "/js/app.bc76fd70.js"
+  },
+  {
+    "revision": "48992f0166bc4f8c168b",
+    "url": "/js/about.9389f1ff.js"
+  },
+  {
+    "revision": "8075359b489b2f18b07c90f7ccdc129a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2869a939aa9bd51c944b902c4c877eb",
+    "url": "/img/local-goods.d2869a93.jpg"
+  },
+  {
+    "revision": "ccb1d7f108416bfd8a4e",
+    "url": "/css/chunk-vendors.0d02966b.css"
+  },
+  {
+    "revision": "445e7f53c89b3dc22884",
+    "url": "/css/app.19592633.css"
+  }
+];
